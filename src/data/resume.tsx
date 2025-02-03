7b0f182731115c0149523316767e7995627fdfbc
pxpi1ot <pxpilot@outlook.com>
2025-02-03 20:12:09 +0800

    修改简介

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
 	location: "Xiamen, Fujian",
 	locationLink: "https://www.google.com/maps/place/sanfrancisco",
 	description:
-		"TS/JS爱好者💻。喜欢使用React⚛️、Next.js🚀构建漂亮的、高效的系统🌟。",
+		"前端开发爱好者💻。喜欢使用React⚛️、Next.js🚀构建漂亮的、高效的系统🌟。",
 	summary:
 		"At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
 	avatarUrl: "/me.png",
